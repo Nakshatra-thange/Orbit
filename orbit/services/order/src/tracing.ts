d117c54661d08e166a5a87ccc57b01695ff00b70
Nakshatra-thange <nakshthange@gmail.com>
2026-06-17 13:59:11 +0530

    ..

---
+++ orbit/services/order/src/tracing.ts
@@ -0,0 +1,21 @@
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'order-service', // change per service
+  }),
+  traceExporter: new JaegerExporter({
+    endpoint: process.env.OTEL_EXPORTER_JAEGER_ENDPOINT
+      ?? 'http://jaeger:14268/api/traces',
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start();
+console.log('[order-service] OpenTelemetry started');
+
+export { sdk };
